6f7141fb45fcc65def13c45961c0a7343accb20e
whystable <mitavi@icloud.com>
2024-08-13 21:30:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 proxy_type = "socks5" #HTTP OR SOCKS5
-proxy_ip = "194.76.138.71"
-proxy_port = "64695"
-proxy_user = "wCM5uUp8"
-proxy_pass = "mzhkB8BL"
+proxy_ip = ""
+proxy_port = ""
+proxy_user = ""
+proxy_pass = ""
